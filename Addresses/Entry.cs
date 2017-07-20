b2919e6053cfe5af9088fae57ac3007e9e1a3a65
Ciao Jyun Pi (Inconsulting Inc.) <v-cipi@microsoft.com>
2017-07-19 22:15:14 -0700

    Add project files.

---
+++ Addresses/Entry.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addresses
+{
+    public class Entry
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CSZ { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public Entry()
+        {
+
+        }
+
+        public Entry(string name, string address, string csz, string phone, string email)
+        {
+            this.Name = name;
+            this.Address = address;
+            this.CSZ = csz;
+            this.Phone = phone;
+            this.Email = email;
+        }
+    }
+
+    public class Entries : List<Entry>
+    {
+        public Entries()
+        {
+            string[] names = { "Bob Anderson", "Bill Henderson", "Charlie Jones", "David King", "Bob Lincoln", "Sam Peterson", "David Roberts", "John Smith", "Steve Thompson", "Bill Williams" };
+            string[] addresses = { "101 Main St", "528 W 32nd", "2938 N 39th St", "2349 E 83rd St", "200 Belmont Pl", "39434 W Smithson Rd", "861 Jones Way NE", "9483 349th St W", "333 33rd W", "1601 Pennsylvania Ave" };
+            string[] cszs = { "Belmont, NC 93849", "San Francisco, CA 28348", "New York, NY 39483", "Seattle, WA 93849", "Bothell, WA 93833", "Kirkland, WA 93322", "Fargo, ND 33293", "New Orleans, LA 22393", "Miami, FL 11928", "Providence, RI 29384" };
+            string[] phones = { "234-304-3290", "403-203-2956", "550-203-3948", "222-309-3093", "854-394-2293", "276-376-2273", "210-204-3948", "345-339-3904", "206-394-4488", "425-394-3933" };
+            string[] emails = { "bob@anderson.com", "bill@henderson.com", "charlie@jones.com", "david@king.com", "bob@lincoln.com", "sam@peterson.com", "david@roberts.com", "john@smith.com", "steve@thompson.com", "bill@williams.com" };
+            for (int i = 0; i < 10; i++)
+            {
+                Entry e = new Entry(names[i], addresses[i], cszs[i], phones[i], emails[i]);
+                Add(e);
+            }
+        }
+        
+
+    }
+
+}
---
+++ Addresses/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace Addresses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Entries AddressList = new Entries();
+        public int current = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Display();
+        }
+
+        public void Display()
+        {
+            Entry e = AddressList[current];
+            tbName.Text = e.Name;
+            tbAddress.Text = e.Address;
+            tbCSZ.Text = e.CSZ;
+            tbPhone.Text = e.Phone;
+            tbEmail.Text = e.Email;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            current = 0;
+            Display();
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e)
+        {
+            current = AddressList.Count - 1;
+            Display();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if(current >0)
+            {
+                current -= 1;
+            }
+            Display();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (current < AddressList.Count - 1)
+            {
+                current += 1;
+            }
+            Display();
+        }
+    }
+}
